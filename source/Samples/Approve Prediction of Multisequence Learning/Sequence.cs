7c0570b5b35ad7c62a3e05eb9acbc3dec024a012
Mahidul Islam <mahidulislamrana003@gmail.com>
2024-02-09 19:46:21 +0100

    Added the Sequence file with some initial Sequence

---
+++ source/Samples/Approve Prediction of Multisequence Learning/Sequence.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System IO;
+
+public class Class1
+{
+	public Class1()
+	{
+        public void rana1()
+        {
+            int[] num = { 1, 2, 3,6 , 8,10, 13 };
+            foreach (int i in num)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
