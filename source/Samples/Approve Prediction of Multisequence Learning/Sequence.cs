c74c5fce73f97e4b8384c1bdc0ac3ff57856505a
Mahidul Islam <mahidulislamrana003@gmail.com>
2024-02-09 22:11:13 +0100

    Sequence learning

--- source/Samples/Approve Prediction of Multisequence Learning/Sequence.cs
+++ source/Samples/Approve Prediction of Multisequence Learning/Sequence.cs
@@ -1,17 +1,40 @@
 ﻿using System;
-using System IO;
 
-public class Class1
+public class SequenceData
 {
-	public Class1()
-	{
-        public void rana1()
-        {
-            int[] num = { 1, 2, 3,6 , 8,10, 13 };
-            foreach (int i in num)
-            {
-                Console.WriteLine(i);
-            }
-        }
+    public SequenceData()
+    {
+        Dictionary<string, List<string>> sequences = new Dictionary<string, List<string>>();
+
+
+        sequences.Add("S1", new List<string>(new string[] { 0.0, 1.0, 2.0, 3.0, 4.0, 2.0, 5.0, 6.0, 7.0 }));
+        sequences.Add("S2", new List<string>(new string[] { 8.0, 1.0, 2.0, 9.0, 10.0, 7.0, 11.00 }));
+        sequences.Add("S3", new List<string>(new string[] { 12.0, 13.0, 14.0, 15.0, 16.0, 6.0, 9.00 }));
+        sequences.Add("S4", new List<string>(new string[] { 3.0, 6.0, 9.0, 12.0, 15.0, 18.0, 21.00 }));
+
+        //
+        // Prototype for building the prediction engine.
+        MultiSequenceLearning experiment = new MultiSequenceLearning();
+        var predictor = experiment.Run(sequences);
+
+        //
+        // These list are used to see how the prediction works.
+        // Predictor is traversing the list element by element. 
+        // By providing more elements to the prediction, the predictor delivers more precise result.
+        var list1 = new string[] { 1.0, 2.0, 3.0, 4.0, 2.0, 5.0 };
+        var list2 = new string[] { 2.0, 3.0, 4.0 };
+        var list3 = new string[] { 8.0, 1.0, 2.0 };
+
+        predictor.Reset();
+        PredictNextElement(predictor, list1);
+
+        predictor.Reset();
+        PredictNextElement(predictor, list2);
+
+        predictor.Reset();
+        PredictNextElement(predictor, list3);
+
+
+
     }
 }
