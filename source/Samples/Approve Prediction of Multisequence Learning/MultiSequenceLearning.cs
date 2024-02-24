0d3330d6b7f13cc7bcc7d2cdae539ef46ffde32a
Mahidul Islam <mahidulislamrana003@gmail.com>
2024-02-24 18:41:44 +0100

    PublicPeotectorRun updated

--- source/Samples/Approve Prediction of Multisequence Learning/MultiSequenceLearning.cs
+++ source/Samples/Approve Prediction of Multisequence Learning/MultiSequenceLearning.cs
@@ -10,6 +10,14 @@ using System.Diagnostics;
 using System.Linq;
 
 
+using NeoCortexApi;
+using NeoCortexApi.Classifiers;
+using NeoCortexApi.Encoders;
+using NeoCortexApi.Entities;
+using NeoCortexApi.Classifiers;
+using NeoCortexApi.Network;
+
+
 namespace NeoCortexApiSample
 {
     /// <summary>
@@ -23,55 +31,64 @@ namespace NeoCortexApiSample
         /// <param name="sequences">Dictionary of sequences. KEY is the sewuence name, the VALUE is th elist of element of the sequence.</param>
         public Predictor Run(Dictionary<string, List<double>> sequences)
         {
-            Console.WriteLine($"Hello NeocortexApi! Experiment {nameof(MultiSequenceLearning)}");
 
+            Console.WriteLine($"Hello NeocortexApi! Experiment {nameof(MultiSequenceLearning)}");
             int inputBits = 100;
             int numColumns = 1024;
-
-            HtmConfig cfg = new HtmConfig(new int[] { inputBits }, new int[] { numColumns })
-            {
-                Random = new ThreadSafeRandom(42),
-
-                CellsPerColumn = 25,
-                GlobalInhibition = true,
-                LocalAreaDensity = -1,
-                NumActiveColumnsPerInhArea = 0.02 * numColumns,
-                PotentialRadius = (int)(0.15 * inputBits),
-                //InhibitionRadius = 15,
-
-                MaxBoost = 10.0,
-                DutyCyclePeriod = 25,
-                MinPctOverlapDutyCycles = 0.75,
-                MaxSynapsesPerSegment = (int)(0.02 * numColumns),
-
-                ActivationThreshold = 15,
-                ConnectedPermanence = 0.5,
-
-                // Learning is slower than forgetting in this case.
-                PermanenceDecrement = 0.25,
-                PermanenceIncrement = 0.15,
-
-                // Used by punishing of segments.
-                PredictedSegmentDecrement = 0.1
-            };
-
-            double max = 20;
-
-            Dictionary<string, object> settings = new Dictionary<string, object>()
-            {
-                { "W", 15},
-                { "N", inputBits},
-                { "Radius", -1.0},
-                { "MinVal", 0.0},
-                { "Periodic", false},
-                { "Name", "scalar"},
-                { "ClipInput", false},
-                { "MaxVal", max}
-            };
-
-            EncoderBase encoder = new ScalarEncoder(settings);
-
+            HtmConfig cfg = HelperMethods.FetchHTMConfig(inputBits, numColumns);
+            EncoderBase encoder = HelperMethods.GetEncoder(inputBits);
             return RunExperiment(inputBits, cfg, encoder, sequences);
+
+
+            /* Console.WriteLine($"Hello NeocortexApi! Experiment {nameof(MultiSequenceLearning)}");
+
+             int inputBits = 100;
+             int numColumns = 1024;
+
+             HtmConfig cfg = new HtmConfig(new int[] { inputBits }, new int[] { numColumns })
+             {
+                 Random = new ThreadSafeRandom(42),
+
+                 CellsPerColumn = 25,
+                 GlobalInhibition = true,
+                 LocalAreaDensity = -1,
+                 NumActiveColumnsPerInhArea = 0.02 * numColumns,
+                 PotentialRadius = (int)(0.15 * inputBits),
+                 //InhibitionRadius = 15,
+
+                 MaxBoost = 10.0,
+                 DutyCyclePeriod = 25,
+                 MinPctOverlapDutyCycles = 0.75,
+                 MaxSynapsesPerSegment = (int)(0.02 * numColumns),
+
+                 ActivationThreshold = 15,
+                 ConnectedPermanence = 0.5,
+
+                 // Learning is slower than forgetting in this case.
+                 PermanenceDecrement = 0.25,
+                 PermanenceIncrement = 0.15,
+
+                 // Used by punishing of segments.
+                 PredictedSegmentDecrement = 0.1
+             };
+
+             double max = 20;
+
+             Dictionary<string, object> settings = new Dictionary<string, object>()
+             {
+                 { "W", 15},
+                 { "N", inputBits},
+                 { "Radius", -1.0},
+                 { "MinVal", 0.0},
+                 { "Periodic", false},
+                 { "Name", "scalar"},
+                 { "ClipInput", false},
+                 { "MaxVal", max}
+             };
+
+             EncoderBase encoder = new ScalarEncoder(settings);
+
+             return RunExperiment(inputBits, cfg, encoder, sequences); */
         }
 
         /// <summary>
